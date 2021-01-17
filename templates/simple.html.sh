22b762f7ff920c9aaae8dea6c336e1a7467f5c11
Anxo Rodriguez <anxolin@gmail.com>
2021-01-17 22:02:32 +0100

    Add emojis and command result code

--- templates/simple.html.sh
+++ templates/simple.html.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 [ ! -z "$TRACE_CONTENT" ] && TRACE_TEXT="
-<h2>TRACE-ERROR OUTPUT</h2>
+<h2>🔎 TRACE-ERROR OUTPUT</h2>
 <pre>
   $TRACE_CONTENT
 </pre>"
+
+# Color RESULT CODE (success/error)
+[[ "$RESULT" == "0" ]] && RESULT_COLOR="#00af91" || RESULT_COLOR="#ff4073"
 
 cat << _EOF_
 <!doctype html>
@@ -24,16 +27,16 @@ cat << _EOF_
     </pre>
 
     <h2>RESULT CODE</h2>
+     <span style="font-size: 16px; background-color: $RESULT_COLOR; color: #fff; padding: 10px; font-weight: 600; margin: 0 0 10px 0; display: inline-block; width: 35px; text-align: center;">
+      $RESULT
+    </span>
+
+    <h2>❌ ERROR OUTPUT</h2>
     <pre>
-    $RESULT
+      $ERR_CONTENT
     </pre>
 
-    <h2>ERROR OUTPUT</h2>
-    <pre>
-    $ERR_CONTENT
-    </pre>
-
-    <h2>STANDARD OUTPUT</h2>
+    <h2>✏️ STANDARD OUTPUT</h2>
     <pre>
       $OUT_CONTENT
     </pre>
@@ -41,5 +44,4 @@ cat << _EOF_
   </body>
 
 </html>
-
 _EOF_
